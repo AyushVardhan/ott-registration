2fdb60484d57a0b7c290d17eb575a8f39cea2c75
Ayush Vardhan <ayushvardhan@Ayushs-MacBook-Pro.local>
2021-01-23 10:36:13 +0530

    Ayush | hande empty response scenario

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,16 @@
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import MediaCard from "./MediaCard";
 import { useState, useEffect } from 'react';
 import configData from "./config.json";
 
 function Home(props) {
 
+    const [dataRecvd, setDataRecvd] = useState(true);
     const [mediaList, setMediaList] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
-            const url = configData.SERVER_URL+`/getMedia?durationInDays=7`;
+            const url = configData.SERVER_URL+`/getMedia?durationInDays=1`;
             console.log("Making call to : " + url);
             await fetch(
                 url , {
@@ -19,7 +20,15 @@ function Home(props) {
                 }
             })
             .then(res => res.json())
-            .then(data => setMediaList(data))
+            .then(data => {
+                if (Object.keys(data).length === 0) {
+                    setDataRecvd(false);
+                    console.log("Data received from server is empty.");
+                } else {
+                    setMediaList(data);
+                    setDataRecvd(true);
+                }
+            })
             .catch(error => console.log(error));
         }
         fetchData();
@@ -28,7 +37,8 @@ function Home(props) {
     return (
         <>
             <Container fluid='md' className='mt-5'>
-                {mediaList.map((media,index) => <MediaCard key={index} mediaDetail={media}/>)}
+                <Row className="justify-content-center"><h1>Week's Entertainment List</h1></Row>
+                {dataRecvd ? mediaList.map((media,index) => <MediaCard key={index} mediaDetail={media}/>) : <Row className="justify-content-center mt-4"><h6>No data found.</h6></Row>}
             </Container>
         </>
     );
